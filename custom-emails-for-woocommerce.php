aa20ee4f0e7437f1572d033f6ee16f92493cc62c
kousikmukherjee <kousikmukherjee.li@gmail.com>
2023-12-18 20:40:51 +0530

    v2.6.3

--- custom-emails-for-woocommerce.php
+++ custom-emails-for-woocommerce.php
@@ -3,7 +3,7 @@
 Plugin Name: Additional Custom Emails for WooCommerce
 Plugin URI: https://wpfactory.com/item/custom-emails-for-woocommerce/
 Description: Add custom emails to WooCommerce.
-Version: 2.6.2
+Version: 2.6.3
 Author: WPFactory
 Author URI: https://wpfactory.com
 Text Domain: custom-emails-for-woocommerce
@@ -30,7 +30,7 @@ if ( 'custom-emails-for-woocommerce.php' === basename( __FILE__ ) ) {
 	}
 }
 
-defined( 'ALG_WC_CUSTOM_EMAILS_VERSION' ) || define( 'ALG_WC_CUSTOM_EMAILS_VERSION', '2.6.2' );
+defined( 'ALG_WC_CUSTOM_EMAILS_VERSION' ) || define( 'ALG_WC_CUSTOM_EMAILS_VERSION', '2.6.3' );
 
 defined( 'ALG_WC_CUSTOM_EMAILS_FILE' ) || define( 'ALG_WC_CUSTOM_EMAILS_FILE', __FILE__ );
 
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: wpcodefactory, algoritmika, anbinder, karzin, omardabbas, kousikmu
 Tags: woocommerce, emails, email, custom email, custom emails, woo commerce
 Requires at least: 4.4
 Tested up to: 6.4
-Stable tag: 2.6.2
+Stable tag: 2.6.3
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -145,7 +145,10 @@ Dive in and discover more tools to empower your WooCommerce Store!
 
 == Changelog ==
 
-= 2.6.2 - 14/12/2023 =
+= 2.6.3 - 18/12/2023 =
+* Fix readme.txt file error.
+
+= 2.6.2 - 18/12/2023 =
 * Update readme.txt file.
 
 = 2.6.1 - 14/12/2023 =
